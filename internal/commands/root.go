101414941d1db53898512f1178da506417055350
mogmog-0110 <fujimiya0110@gmail.com>
2026-06-17 11:18:10 +0900

    fix: ひな型のポンボタンをボールの落とし直し+音に変更、ボールのてかり(ハイライト)を削除、着地でパーティクル

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -17,7 +17,7 @@ const (
 // cliVersion は mitiru CLI 自身の版。goreleaser が release 時に ldflags
 // (-X .../commands.cliVersion=<tag>) で上書きする。手元 build では既定値のまま。
 // self-update がこの値と最新 release を比較する (ADR 0010)。
-var cliVersion = "0.12.6"
+var cliVersion = "0.12.7"
 
 func NewRootCommand() *cobra.Command {
 	// 前回の self-update が残した <exe>.old を best-effort で掃除する (ADR 0010 #8)。
